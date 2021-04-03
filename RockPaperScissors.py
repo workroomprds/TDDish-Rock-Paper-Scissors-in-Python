3ca423fbc1d843e4992208852b69325e75d96610
James Lyndsay <jdl@workroom-productions.com>
2021-04-03 14:16:12 +0100

    Removed references to tests (they're in the modules) Pulled comments on what to do next together in one space Took decision on winner out of print announcment for clarity

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
 
 ### CONFIGURATION
-
-# Consider making each ruleset an object, with rules, validAbbreviations, decideWinner, acceptInput
-
-
-# Rock Paper Scissors
 import rulesRPS as rulesRPS
 import rulesRPSLS as rulesRPSLS
 import utilsRPS as utils
 
-# consider a dict (for each ruleset) of viable abbreviations -> values
+#rules = rulesRPS.rules
+rules = rulesRPSLS.rules
 
-rules = rulesRPS.rules
-testMe = True
-
-	
-if (testMe): #all these are called within the modules, so this is not needed...
-	utils.test()
-	rulesRPS.testData()
-	rulesRPSLS.testData()
-	utils.testRules(rulesRPS.rules)
-	utils.testRules(rulesRPSLS.rules)
-	
 ### Initialise
 firstPlayerChoice = ""
 secondPlayerChoice = ""
@@ -30,7 +15,8 @@ validInputForGame = utils.getKeysFrom(rules)
 
 
 ### Main
-
+#### Consider making each ruleset an object, with rules, validAbbreviations, decideWinner, acceptInput
+#### consider a dict (for each ruleset) of viable abbreviations -> values
 #### Consider putting this in a loop.
 #### Consider keping score
 #### Consider an algorothmic opponent (and how to convince that it's not just reading the user input /before/ throwing hand)
@@ -41,4 +27,6 @@ while not (utils.acceptInput(validInputForGame, firstPlayerChoice) ):
 while not (utils.acceptInput(validInputForGame, secondPlayerChoice) ):
 	secondPlayerChoice = input(utils.buildInputRequest("Second", validInputForGame) )
 	
-print(utils.buildAnnouncement(utils.decideWinner(rules, firstPlayerChoice, secondPlayerChoice), firstPlayerChoice, secondPlayerChoice))	
+winner = utils.decideWinner(rules, firstPlayerChoice, secondPlayerChoice)
+
+print(utils.buildAnnouncement(winner, firstPlayerChoice, secondPlayerChoice))	
