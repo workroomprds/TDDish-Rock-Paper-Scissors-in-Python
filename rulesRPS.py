88ad0f7ff2a20b897c2d88cbe0b4af37d95bc72b
James Lyndsay <jdl@workroom-productions.com>
2021-04-05 19:45:26 +0100

    Changed rulesets to work with Game class

--- rulesRPS.py
+++ rulesRPS.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import gameRPS as game
+from gameRPS import Game
 
 # Expressed as a dict of dicts: first:second:result
 rules = {
@@ -11,7 +11,8 @@ rules = {
 
 def test_data():
    """Tests the rules for specific stuff for this ruleset"""
-   validInput = game.valid_input_for_game(rules)
+   game = Game(rules)
+   validInput = game.valid_input_for_game()
    
    # Check that the possible inputs are RPS, and only that.
    assert (validInput[0] == "Rock")
@@ -20,9 +21,9 @@ def test_data():
    assert (validInput.__len__() == 3), "Number of keys"
    
    # Check sample of rules (note: draws and symmetry already checked)
-   assert (game.decide_winner(rules, "Rock", "Paper") == "Second"), "Paper should beat Rock"
-   assert (game.decide_winner(rules, "Rock", "Scissors") == "First"), "Rock should beat Scissors"
-   assert (game.decide_winner(rules, "Paper", "Scissors") == "Second"), "Paper should beat Scissors"
+   assert (game.decide_winner("Rock", "Paper") == "Second"), "Paper should beat Rock"
+   assert (game.decide_winner("Rock", "Scissors") == "First"), "Rock should beat Scissors"
+   assert (game.decide_winner("Paper", "Scissors") == "Second"), "Paper should beat Scissors"
 
    game.testRules(rules)
 
