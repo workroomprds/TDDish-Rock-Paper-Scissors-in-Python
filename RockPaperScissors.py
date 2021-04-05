898dc29b7128b13bb47301ac4b4da3c3e4b882bd
James Lyndsay <jdl@workroom-productions.com>
2021-04-05 19:44:55 +0100

    Changed gameRPS to be a class - moving calling the tests out of the class, and importing only the class in RockPaperScissors Split out the message-making parts

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -7,25 +7,13 @@ import rulesRPS as rulesRPS
 import rulesRPSLS as rulesRPSLS
 import utilsRPS as utils
 import uiRPS as ui
-import gameRPS as game
+from gameRPS import Game
+#import gameRPS as gameRPS
 
 ### Initialise
-#rules = rulesRPS.rules
-rules = rulesRPSLS.rules
-
-### Main
-#### Consider making each ruleset an object, with rules, validAbbreviations, decideWinner, acceptInput
-#### consider a dict (for each ruleset) of viable abbreviations -> values
-#### Consider putting this in a loop.
-#### Consider keping score
-#### Consider an algorithmic opponent (and how to convince that it's not just reading the user input /before/ throwing hand)
-
-firstPlayerChoice  = game.request_valid_input(player="First",  rules=rules, get_input=ui.askForInput)
-secondPlayerChoice = game.request_valid_input(player="Second", rules=rules, get_input=ui.askForInput)
-
-winner = game.decide_winner(rules=rules, first=firstPlayerChoice, second=secondPlayerChoice)
-
-print(game.build_announcement(winner=winner, first=firstPlayerChoice, second=secondPlayerChoice))
+#game = Game(rulesRPS.rules)
+game = Game(rulesRPSLS.rules)
+game.set_user_input_fn(ui.askForInput)
 
 def test():
 	"""To run tests on modules"""
@@ -33,5 +21,20 @@ def test():
 	rulesRPSLS.test_data()
 	utils.test()
 	game.test()
+	
+### Main
+#### Consider making each ruleset an object, with rules, validAbbreviations, decideWinner, acceptInput
+#### consider a dict (for each ruleset) of viable abbreviations -> values
+#### Consider putting this in a loop.
+#### Consider keping score
+#### Consider an algorithmic opponent (and how to convince that it's not just reading the user input /before/ throwing hand)
+
+firstPlayerChoice  = game.request_valid_input(player="First")
+secondPlayerChoice = game.request_valid_input(player="Second")
+
+winner = game.decide_winner(first=firstPlayerChoice, second=secondPlayerChoice)
+
+print(game.build_announcement(winner=winner, first=firstPlayerChoice, second=secondPlayerChoice))
+
 	
 	
